23aa4d87c047fc7ef9d19fd39d2acbcfda446357
kobbin003 <dkfeto@gmail.com>
2025-09-04 19:26:49 +0530

    nextjs config for deployment with pupeeter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+
+	// The `serverExternalPackages` option allows you to opt-out of bundling dependencies in your Server Components.
+	serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
